409fb3c5d509196cade44e1df482e0b3789abfb3
iFyn_ <82264369+i-Fyn@users.noreply.github.com>
2024-12-05 16:20:32 +0800

    Update geely.js

--- js/geely.js
+++ js/geely.js
@@ -135,9 +135,31 @@ async function signIn() {
     //     deviceUUID: $.devicesn,
     //     geelyDeviceId: $.devicesn
     // }
+    sweet_security_info = {
+    "osVersion": "18.2",
+    "ip": "192.168.2.1",
+    "geelyDeviceId": $.devicesn,
+    "deviceUUID": $.devicesn,
+    "brand": "Apple",
+    "appVersion": $.appversion,
+    "channel": "ios%E5%AE%98%E6%96%B9",
+    "ua": "Mozilla\\/5.0 (iPhone; CPU iPhone OS 18_2 like Mac OS X) AppleWebKit\\/605.1.15 (KHTML, like Gecko) Mobile\\/15E148",
+    "isSetProxy": "false",
+    "isUsingVpn": "false",
+    "os": "iOS",
+    "isLBSEnabled": "false",
+    "platform": "ios",
+    "isJailbreak": "false",
+    "networkType": "NETWORK_WIFI",
+    "battery": "80",
+    "os_version": "18.2",
+    "isCharging": "2",
+    "model": "iPhone17,2",
+    "screenResolution": "1290 * 2796"
+}
     headers = Object.assign(getHeaders(), {
         "X-Data-Sign": sign,
-        "sweet_security_info": `{"osVersion":"18.2","geelyDeviceId":"${$.devicesn}","deviceUUID":"${$.devicesn}","brand":"Apple","appVersion":"${$.appversion}","channel":"ios%E5%AE%98%E6%96%B9","ua":"Mozilla\/5.0 (iPhone; CPU iPhone OS 18_2 like Mac OS X) AppleWebKit\/605.1.15 (KHTML, like Gecko) Mobile\/15E148","isSetProxy":"false","isUsingVpn":"false","os":"iOS","isLBSEnabled":"false","platform":"ios","isJailbreak":"false","networkType":"none","battery":"80","os_version":"18.2","isCharging":"2","model":"iPhone17,2","screenResolution":"1290 * 2796"}`
+        "sweet_security_info": `{"osVersion":"18.2","ip":"192.168.2.1","geelyDeviceId":"${$.devicesn}","deviceUUID":"${$.devicesn}","brand":"Apple","appVersion":"${$.appversion}","channel":"ios%E5%AE%98%E6%96%B9","ua":"Mozilla\\/5.0 (iPhone; CPU iPhone OS 18_2 like Mac OS X) AppleWebKit\\/605.1.15 (KHTML, like Gecko) Mobile\\/15E148","isSetProxy":"false","isUsingVpn":"false","os":"iOS","isLBSEnabled":"false","platform":"ios","isJailbreak":"false","networkType":"NETWORK_WIFI","battery":"80","os_version":"18.2","isCharging":"2","model":"iPhone17,2","screenResolution":"1290 * 2796"}`
     });
     const rest = { url, body, headers }
     let { code, data, message } = await httpRequest(rest);
